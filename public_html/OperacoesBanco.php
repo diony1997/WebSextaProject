d8eecc694ae8bd02fdb31f1b6d886cc141ebb494
guivmc <guivmc@hotmail.com>
2019-05-21 10:52:23 -0300

    Update OperacoesBanco.php

--- public_html/OperacoesBanco.php
+++ public_html/OperacoesBanco.php
@@ -1,10 +1,12 @@
 <?php
+
 require_once './Util/DataSource.php';
 require_once './Util/Usuario.php';
 
 class OperacoesBanco {
+
     private $linkDB;
-    
+
     function __construct() {
         $this->linkDB = new DataSource();
     }
@@ -12,97 +14,120 @@ class OperacoesBanco {
     function __destruct() {
         $this->linkDB = NULL;
     }
-    
+
     function inserirUser($obj) {
         $sql = "INSERT INTO user (nome, email, senha, imageURL) VALUES(?, ?, ?, ?)";
-        
+
         //tratamento do sql injection
         $stm = mysqli_prepare($this->linkDB->con, $sql);
-        
-        if(!$stm) {
+
+        if (!$stm) {
             die('Falha na construcao SQL');
         }
-        
+
         $nome = $obj->getNome();
         $email = $obj->getEmail();
         $senha = $obj->getSenha();
         $imagem = $obj->getImagemURL();
-                
-        if(!$stm->bind_param("ssss", $nome, $email, $senha, $imagem)) {
+
+        if (!$stm->bind_param("ssss", $nome, $email, $senha, $imagem)) {
             die('Falha na atribuicao de valores');
         }
-        
-        return $stm->execute();       
-    }
-    
-    function getUser($id){
-        $resultado = mysqli_query($this->linkDB->con, "SELECT * FROM usuario WHERE id = ".$id);
-        
-        if ($resultado && mysqli_num_rows($resultado) == 1){
-            $dados = mysqli_fetch_assoc($resultado);
-            $user = new Usuario($dados['email'], $dados['senha'], $dados['nome']);
-            $user->setImagemURL($dados['imagemURL']);
-            $user->setId($dados['id']);
-            return $user;
-        }else{
-            return null;
-        }
-    }
-    
-    function itemExiste($campo, $item) {
-        $resultado = mysqli_query($this->linkDB->con, "SELECT * FROM usuario WHERE ".$campo." = '".$item."'");
 
-        return $resultado && mysqli_num_rows($resultado) > 0;
-    }
-    
-    function update($id, $campo, $valor){
-        $sql = "UPDATE usuario SET ".$campo." = ? WHERE ID = ".$id;
-        
-        //tratamento do sql injection
-        $stm = mysqli_prepare($this->linkDB->con, $sql);
-        
-        if(!$stm) {
-            return false;
+        if (!$stm->execute()) {
+            die('Falha na execucao SQL');
         }
-                
-        if(!$stm->bind_param("s", $valor)) {
-            return false;
-        }
-        
-        return $stm->execute();       
     }
-    
-    function login($email, $senha){
-        
-        $resultado = mysqli_prepare($this->linkDB->con, "SELECT * FROM user WHERE email = ? AND senha = ?");
-        
+
+    function getUser($id) {
+
+        $resultado = mysqli_prepare($this->linkDB->con, "SELECT * FROM user WHERE id = ?");
+
         if (!$resultado) {
             die('Falha na construcao SQL');
         }
-        
+
+        if (!$resultado->bind_param("s", $id)) {
+            die('Falha na atribuicao de valores');
+        }
+
+        if (!$resultado->execute()) {
+            die('Falha na execucao SQL');
+        }
+
+        $result = $resultado->get_result();
+
+        if ($result->num_rows === 1) {
+            $dados = mysqli_fetch_assoc($result);
+            $user = Array('email' => $dados['email'], 'imageURL' => $dados['imageURL'], 'nome' => $dados['nome'], 'id' => $dados['id']);
+            return $user;
+        }
+
+        return null;
+    }
+
+    function update($id, $campo, $valor) {
+
+        $resultado = mysqli_prepare($this->linkDB->con, "UPDATE user SET ? = ? WHERE ID = ?");
+
+        //tratamento do sql injection
+        if (!$resultado) {
+            die('Falha na construcao SQL');
+        }
+
+        if (!$resultado->bind_param("sss", $campo, $valor, $id)) {
+            die('Falha na atribuicao de valores');
+        }
+
+        if (!$resultado->execute()) {
+            die('Falha na execucao SQL');
+        }
+    }
+
+    function login($email, $senha) {
+
+        $resultado = mysqli_prepare($this->linkDB->con, "SELECT * FROM user WHERE email = ? AND senha = ?");
+
+        if (!$resultado) {
+            die('Falha na construcao SQL');
+        }
+
         if (!$resultado->bind_param("ss", $email, $senha)) {
             die('Falha na atribuicao de valores');
         }
-        
-          if (!$resultado->execute()) {
+
+        if (!$resultado->execute()) {
             die('Falha na execucao SQL');
         }
-        
+
         $result = $resultado->get_result();
-            
-        if ($result->num_rows ===  1){
+
+        if ($result->num_rows === 1) {
             $dados = mysqli_fetch_assoc($result);
-            $user = Array('email' => $dados['email'], 'imageURL' => $dados['imageURL'],'nome' => $dados['nome'], 'id' => $dados['id']);
+            $user = Array('email' => $dados['email'], 'imageURL' => $dados['imageURL'], 'nome' => $dados['nome'], 'id' => $dados['id']);
             return $user;
-        } 
-        
+        }
+
         return null;
-        
     }
-    
-    function deleteUser($id){
-        $resultado = mysqli_query($this->linkDB->con, "DELETE FROM user WHERE id = ".$id);
+
+    function deleteUser($id) {
+
+        $resultado = mysqli_prepare($this->linkDB->con, "DELETE FROM user WHERE id = ? ");
+
+        if (!$resultado) {
+            die('Falha na construcao SQL');
+        }
+
+        if (!$resultado->bind_param("s", $id)) {
+            die('Falha na atribuicao de valores');
+        }
+
+        if (!$resultado->execute()) {
+            die('Falha na execucao SQL');
+        }
+
         return $resultado;
     }
-    
+
 }
